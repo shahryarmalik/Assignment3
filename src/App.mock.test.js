e36f14873f57c571584d2aa0a87570269f3b36a8
Eric Bush <44658934+Blueskyproductions@users.noreply.github.com>
2021-03-29 17:29:50 -0600

    Changes some comments about the mock usage

--- src/App.mock.test.js
+++ src/App.mock.test.js
@@ -41,6 +41,12 @@ describe('Test Suite for App Component', () => {
 });
 
 //XXXXXXXXXXXXXXXXXXXXXXXXXX
+// To mock a function of a returned module like the os module of Node.js
+// jest.mock('os', () => {
+//   return { hostname: () => 'my-computer' }
+// })
+
+
 // import { loadStripe } from '@stripe/stripe-js';
 // jest.mock('@stripe/stripe-js', () => {
 //   return {
@@ -51,37 +57,48 @@ describe('Test Suite for App Component', () => {
 //   };
 // });
 
+// Shows mock of the aws-sdk SQS object and functions found on that.
 // var AWS = require('aws-sdk');
-// jest.mock('aws-sdk', () => {
-//   const SQSMocked = {
-//     receiveMessage: jest.fn((paramscancel, cb) => {
-//       // throw new Error('Test error');
-//       setImmediate(() => {
-//         return cb(null,
-//           {
-//             "ResponseMetadata": {
-//               "RequestId": "7b2d9ba3-4395-5d9a-8d2f-ccc460220805"
-//             }
-//           });
-//       })
-//     }),
-//     sendMessage: jest.fn((params, cb) => {
-//       // throw new Error('Test error');
-//       setImmediate(() => {
-//         return cb(null,
-//           {
-//             "ResponseMetadata": {
-//               "RequestId": "f38f44a5-f10c-59ac-8300-13c2d2c2c678"
-//             },
-//             "MD5OfMessageBody": "80148b94de01a2a26976475590570f3c",
-//             "MessageId": "624b39d0-3ff4-48b5-a297-19a5158d7a9b",
-//             "SequenceNumber": "18854449398826223616"
-//           });
-//       })
+// const FakeSQS = jest.fn(() => ({
+//   receiveMessage: jest.fn((paramscancel, cb) => {
+//     // throw new Error('Test error');
+//     setImmediate(() => {
+//       return cb(null,
+//         {
+//           "ResponseMetadata": {
+//             "RequestId": "7b2d9ba3-4395-5d9a-8d2f-ccc460220805"
+//           }
+//         });
 //     })
-//   };
-//   return {
-//     SQS: jest.fn(() => SQSMocked),
-//     config: { update: jest.fn(() => "called config") }
-//   };
+//   }),
+//   sendMessage: jest.fn((params, cb) => {
+//     // throw new Error('Test error');
+//     setImmediate(() => {
+//       return cb(null,
+//         {
+//           "ResponseMetadata": {
+//             "RequestId": "f38f44a5-f10c-59ac-8300-13c2d2c2c678"
+//           },
+//           "MD5OfMessageBody": "80148b94de01a2a26976475590570f3c",
+//           "MessageId": "624b39d0-3ff4-48b5-a297-19a5158d7a9b",
+//           "SequenceNumber": "18854449398826223616"
+//         });
+//     })
+//   })
+// }));
+// AWS.SQS = FakeSQS;
+
+// // now in the real code deeper in some file called from a test, the following gets the mock
+// var AWS = require('aws-sdk');
+// AWS.config.update({
+//   accessKeyId: process.env.AWS_ACCESS_KEY_ID_2,
+//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_2,
+//   region: 'us-east-1'
 // });
+// sqs = new AWS.SQS();
+// sqs.sendMessage(params, (error, data) => { ...
+// sqs.receiveMessage(paramscancel, function (err, data) { ...
+
+// // Back in the test code
+// jest.clearAllMocks();
+
