fae02e336a05cff6bed44d581de86556038e5799
Eric Bush <44658934+Blueskyproductions@users.noreply.github.com>
2021-04-02 15:45:59 -0600

    Adjusted the Axios mocking example

--- src/swapiGetter.js
+++ src/swapiGetter.js
@@ -3,6 +3,6 @@ import axios from 'axios'
 
 // Get a star wars person by id and return their name
 const swapiGetter = id =>
-     axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`).then(res => res.data.title)
+     axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`).then(res => res.data)
 
 export default swapiGetter
