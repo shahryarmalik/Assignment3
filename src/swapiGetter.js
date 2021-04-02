7d0d79793910904734ba51f4927d68518f6a77d0
Bill Hodghead <bhcrosslake@users.noreply.github.com>
2021-04-02 15:34:29 -0700

    del old files

--- src/swapiGetter.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-//const mockAxios = require('axios');
-
-// Get a star wars person by id and return their name
-const swapiGetter = id =>
-     axios.get(`https://jsonplaceholder.typicode.com/todos/${id}`).then(res => res.data)
-
-export default swapiGetter
--- src/swapiGetter.test.js
+++
@@ -1,7 +0,0 @@
-import swapiGetter from "./swapiGetter";
-
-test('should return the first name from the api', async () => {
-    const result = await swapiGetter(1);
-    expect(result.title).toBe('delectus aut autem');
-});
-
